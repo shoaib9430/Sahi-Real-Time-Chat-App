4cc4a4388a50c35c7670f1347a76435707a5db76
Shoaib Akhtar <shoaibakhtar9430@gmail.com>
2023-08-21 14:57:28 +0530

    first last

--- backend/server.js
+++ backend/server.js
@@ -21,17 +21,13 @@ app.use("/api/message", messageRoutes);
 
 
 //deployment
-const __dirname1 = path.resolve();
+const path = require("path");
+__dirname = path.resolve();
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname1, "/frontend/build")));
-
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
-  );
-} else {
-  app.get("/", (req, res) => {
-    res.send("API is running..");
+  app.use(express.static(path.join(__dirname, "/client/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
   });
 }
 
